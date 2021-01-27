1d50add26df5e41f5b2a96913e276424a2292c94
justKD <cady.holmes.dev@gmail.com>
2021-01-27 12:18:12 -0600

    added template

---
+++ tests/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @file tests/index.ts
+ * @author Cadence Holmes
+ * @copyright Cadence Holmes 2020
+ * @license MIT
+ * @fileoverview
+ * Main entry point to the test suite.
+ * Import this folder to add the test suite.
+ */
+
+import './_tests.setup';
